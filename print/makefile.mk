f139856ae7307f062e17816d6369d62a33f51b91
Daniel Kiss <littlesnorrboy@gmail.com>
2017-06-03 12:05:36 +0200

    Trying trying trying

--- print/makefile.mk
+++ print/makefile.mk
@@ -2,10 +2,10 @@ SRC = $(wildcard *.cpp)
 CXX = clang
 LDFLAGS=-g -std=c++14 -stdlib=libc++ -lstdc++
 
-COMPILE_EXAMPLE = $(OBJS) $(CXX) example.cpp logservice.cpp $(LDFLAGS) -o example.exe $(OBJS)
 
 all: example
 
+COMPILE_EXAMPLE = $(OBJS) $(CXX) example.cpp logservice.cpp $(LDFLAGS) -o example.exe $(OBJS)
 run:
 	$(COMPILE_EXAMPLE) && ./example.exe
 
